6cedf954ee2e61d748dcf211dce7772c95f3297b
alycano <aly.cano@udea.edu.co>
2025-11-13 22:43:08 -0500

    feat: función sin parámetros y sin retorno implementada

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Proyecto Funciones en Java ===");
-        // Aquí iremos llamando las funciones a medida que las implementes.
+        // Llamada a la función sin parámetros y sin retorno
+        funcionSinParametrosSinRetorno();
+    }
+
+    // 1. Función sin parámetros y sin retorno
+    public static void funcionSinParametrosSinRetorno() {
+        System.out.println("Esta es una función sin parámetros y sin retorno.");
     }
 }
