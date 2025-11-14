ad5768435fb4d5e3501e9eecf4201aa9c01d951d
alycano <aly.cano@udea.edu.co>
2025-11-14 11:26:46 -0500

    feat: función con parámetros y sin retorno implementada

--- Main.java
+++ Main.java
@@ -4,10 +4,18 @@ public class Main {
 
         // Llamada a la función sin parámetros y sin retorno
         funcionSinParametrosSinRetorno();
+
+        // Llamada a la función con parámetros y sin retorno
+        funcionConParametrosSinRetorno("Santiago", 31);
     }
 
     // 1. Función sin parámetros y sin retorno
     public static void funcionSinParametrosSinRetorno() {
         System.out.println("Esta es una función sin parámetros y sin retorno.");
+    }
+
+    // 2. Función con parámetros y sin retorno
+    public static void funcionConParametrosSinRetorno(String nombre, int edad) {
+        System.out.println("Hola " + nombre + ". Tienes " + edad + " años.");
     }
 }
